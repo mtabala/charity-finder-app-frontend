7a3c52fcd98dfb951451d45def2bcbdac4976c1b
Masha <mariia.tabala@gmail.com>
2023-03-16 18:38:15 -0700

    import browser router, routes and route

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
