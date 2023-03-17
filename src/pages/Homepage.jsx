06ee61d106baf78ce3c2e5c7e96ec1a6c47e005c
Masha <mariia.tabala@gmail.com>
2023-03-17 12:58:37 -0700

    define api, define function to get all organizations with axios, define useEffect to get organizations from json api.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,19 +1,37 @@
 import "./Homepage.scss";
-// import Header from "../components/Header/Header";
-// import { useState, useEffect } from "react";
-// import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Hero from "../components/Hero/Hero";
-// import Footer from "../components/Footer/Footer";
-// import CharityList from "../components/CharityList/CharityList";
-// import Footer from "../components/Footer/Footer";
-// import axios from "axios";
+import axios from "axios";
 
 function Homepage() {
-  return (
-    <div className="homepage">
-      <Hero />
-    </div>
-  );
+    //Define API
+    const api = "http://localhost:5000";
+
+    //Define use state variables using useState hooks
+    const [organizations, setOrganizations] = useState([]); //all organizations
+
+    // Define useEffect to get all organizations from our json api
+    useEffect(() => {
+        getOrganizations();
+    }, []);
+
+    // Define function to get all videos from the API
+    function getOrganizations() {
+        axios
+            .get(`${api}/organization`)
+            .then((response) => {
+                setOrganizations(response.data);
+            })
+            .catch((err) => {
+                console.log("err: ", err);
+            })
+    };
+
+    return (
+        <div className="homepage">
+            <Hero />
+        </div>
+    );
 }
 
 export default Homepage;
