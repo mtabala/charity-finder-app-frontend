dfbe05f20f2520ee6702dcfdf93e58a0bfb44ca5
Genesis Vivas <genesis.vivasb@gmail.com>
2023-03-17 13:30:35 -0700

    Continue styling page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,12 @@
 import "./Header.scss";
-import logo from "../../assets/images/hand-care-logo.jpg";
-import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo-transparent.svg";
 
 function Header() {
   return (
     <header className="header">
-      <Link className="logo" to="/" src={logo}>
-        Logo
-      </Link>
       <nav className="nav">
+        <img className="nav__logo" src={logo} alt="logo" />
+        <p className="nav__title">Kemarogi</p>
         <ul className="nav__list">
           <li className="nav__list-item">Charities</li>
           <li className="nav__list-item">Partners</li>
