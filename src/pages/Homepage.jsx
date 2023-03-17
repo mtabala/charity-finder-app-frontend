c5b9f3afde92a89c22df41ac09a8196a8459c2e1
Masha <mariia.tabala@gmail.com>
2023-03-17 13:55:07 -0700

    define a useEffect to fetch a single org.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,19 +1,48 @@
 import "./Homepage.scss";
 import { useState, useEffect } from "react";
+import { useParams } from "react-router";
 import Hero from "../components/Hero/Hero";
 import axios from "axios";
 
 function Homepage() {
     //Define API
     const api = "http://localhost:5000";
-
+    console.log("masha");
     //Define use state variables using useState hooks
     const [organizations, setOrganizations] = useState([]); //all organizations
+    const [currentOrganization, setCurrentOrganization] = useState({}) //selected organization
+    const { charityId } = useParams();
+
+    function getRandomOrg(orgListLen) {
+        return Math.floor((Math.random() * orgListLen));
+    }
 
     // Define useEffect to get all organizations from our json api
     useEffect(() => {
         getOrganizations();
     }, []);
+
+    useEffect(() => {
+        if (charityId) {
+            getCurrentOrg(charityId);
+        } else if (organizations.length) {
+            let randomOrg = getRandomOrg(organizations.length);
+            let currentOrg = organizations[randomOrg].id;
+            getCurrentOrg(currentOrg);
+        }
+    }, [charityId, organizations])
+
+    function getCurrentOrg(orgId) {
+        const url = `http://localhost:5000/organization/${orgId}}`;
+        axios.get(url)
+            .then(response => {
+                setCurrentOrganization(response.data)
+                console.log(response)
+            })
+            .catch(err => {
+                console.log(err, "could not find the requested organization");
+            })
+    }
 
     // Define function to get all videos from the API
     function getOrganizations() {
@@ -21,6 +50,7 @@ function Homepage() {
             .get(`${api}/organization`)
             .then((response) => {
                 setOrganizations(response.data);
+                console.log(response.data)
             })
             .catch((err) => {
                 console.log("err: ", err);
