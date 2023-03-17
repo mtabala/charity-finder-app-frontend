7083d3048b556892dcac67685ef7921833b2a0ce
Masha <mariia.tabala@gmail.com>
2023-03-16 18:46:37 -0700

    import browser router, routes, route

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
@@ -34,7 +10,6 @@ function App() {
             </Routes>
         </BrowserRouter>
     );
->>>>>>> feature/masha
 }
 
 export default App;
